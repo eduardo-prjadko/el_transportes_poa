b2f17c32d02da51c905954f9c3bcd6e88bb2ea90
Eduardo Prjadko <ed@debian.prjadko>
2021-07-26 20:36:11 -0300

    new el_orchestrator

--- el_orchestrator/__init__.py
+++ el_orchestrator/__init__.py
@@ -9,32 +9,14 @@ def orchestrator_function(context: df.DurableOrchestrationContext):
     
     env = get_env_var()
 
-    # first extract call
+    # first extract
     f_data = yield context.call_activity('activity_extract', '')
     f_data['result']['offset'] = 0
 
     # subsequent extract calls
-    pages = math.ceil(f_data['result']['total'] / 100)
-    i = 1
+    offset_list = list(range(100, f_data['result']['total'], 100))
     tasks = []
-    offset = 100
-    while i <= pages:
-        endpoint = f'/api/3/action/datastore_search?offset={str(offset)}&resource_id=cb96a73e-e18b-4371-95c5-2cf20e359e6c'
-        tasks.append(context.call_activity('activity_extract', endpoint))
-        i += 1
-        offset += 100
-    data = yield context.task_all(tasks)
-    data.append(f_data)
-
-    # upload to storage
-    tasks = []
-    for item in data:
-        params = {
-            'container': env.CONTAINER, 
-            'data': item,
-            'index': item['result']['offset']
-        }
-        tasks.append(context.call_activity('activity_upload', input_=params))
-    yield context.task_all(tasks)
+    for offset in offset_list:
+        tasks.append(context.call_activity('el_suborchestrator', offset))
 
 main = df.Orchestrator.create(orchestrator_function)
