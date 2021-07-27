7e9af35e05bf3ea172d4604101d2673df19eb665
Eduardo Prjadko <ed@debian.prjadko>
2021-07-26 23:41:57 -0300

    change variable

--- activity_extract/__init__.py
+++ activity_extract/__init__.py
@@ -1,18 +1,12 @@
 from instances import get_session
-from instances import get_env_var
 
 
-URL = 'https://dadosabertos.poa.br'
-
-def main(endpoint: str) -> dict:
+def main(url: str) -> dict:
     # instantiating request session 
     session = get_session()
 
-    if endpoint == '':
-        endpoint = '/api/3/action/datastore_search?resource_id=cb96a73e-e18b-4371-95c5-2cf20e359e6c'
-
     # get data
-    r = session.get(f'{URL}{endpoint}')
+    r = session.get(url)
 
     # if request fails, it raises an error
     if r.status_code != 200:
