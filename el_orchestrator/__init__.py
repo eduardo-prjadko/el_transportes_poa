3a4afb6946e8fd4d4f4f018cfd4b03ddad4156d0
Eduardo Prjadko <ed@debian.prjadko>
2021-07-26 23:42:31 -0300

    refactored for calling suborchestrator

--- el_orchestrator/__init__.py
+++ el_orchestrator/__init__.py
@@ -1,22 +1,32 @@
-import math
-
 import azure.durable_functions as df
 
 from instances import get_env_var
 
 
 def orchestrator_function(context: df.DurableOrchestrationContext):
-    
+
     env = get_env_var()
 
-    # first extract
-    f_data = yield context.call_activity('activity_extract', '')
-    f_data['result']['offset'] = 0
+    # first extract and load
+    host = env.HOST
+    limit = env.LIMIT
+    endpoint = env.ENDPOINT.substitute(offset=str(0), limit=str(limit))
+    data = yield context.call_activity('activity_extract', f'{host}{endpoint}')
+    params = {
+        'container': env.CONTAINER, 
+        'data': data,
+        'index': 0
+    }
+    yield context.call_activity('activity_upload', input_=params)
 
-    # subsequent extract calls
-    offset_list = list(range(100, f_data['result']['total'], 100))
+    # suborchestrator call
+    offset_list = list(range(int(limit), data['result']['total'], int(limit)))
     tasks = []
     for offset in offset_list:
-        tasks.append(context.call_activity('el_suborchestrator', offset))
+        tasks.append(context.call_sub_orchestrator('el_suborchestrator', offset ))
+    
+    yield context.task_all(tasks)
+
+    return 'succeeded'
 
 main = df.Orchestrator.create(orchestrator_function)
